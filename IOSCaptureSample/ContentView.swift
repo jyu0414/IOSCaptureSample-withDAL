669060c1b10cfb391b22953b7f7f39504d50b843
Yuji Sasaki <jyu0414@gmail.com>
2021-05-11 04:22:14 +0900

    Initial Commit

---
+++ IOSCaptureSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOSCaptureSample
+//
+//  Created by Yuji Sasaki on 2021/05/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOSCaptureSample/IOSCaptureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSCaptureSampleApp.swift
+//  IOSCaptureSample
+//
+//  Created by Yuji Sasaki on 2021/05/11.
+//
+
+import SwiftUI
+
+@main
+struct IOSCaptureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
